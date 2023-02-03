ce491e027685b12a72e78938c19d209ff304c515
Frank Tramontin <frankdavi@gmail.com>
2023-02-03 20:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ contas_divisao/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contas_divisao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(txtValor1.Text);
+            int valor2 = Convert.ToInt32(txtValor2.Text);
+            int resultado = valor1 - valor2;
+            txtResultado.Text = resultado.ToString();
+        }
+    }
+}
